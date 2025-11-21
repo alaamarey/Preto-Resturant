2f0134b42aed0b89e9b1ed30855ac3c4b9653a32
alaa marey <alaamarey616@gmail.com>
2025-11-21 13:00:06 +0200

    update environment in production phase

--- src/core/auth/register/services/register.service.ts
+++ src/core/auth/register/services/register.service.ts
@@ -3,19 +3,16 @@ import { API_URL } from '../../../../token';
 import { Observable } from 'rxjs';
 import { Register } from '../models/register.interface';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
   
-
-
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    console.log(environment.apiUrl);
+    console.log(this.API_URL);
     
   }
 
